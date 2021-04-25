3d3e68def8b8f6966423d1d03a4c8b6843b5d679
HashimAli1996 <hashim20070@hotmail.co.uk>
2021-04-25 09:37:07 +0100

    AC1 complete

--- MMTDigitalTechTest/PageObjects/LoginPage.cs
+++ MMTDigitalTechTest/PageObjects/LoginPage.cs
@@ -2,12 +2,13 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
+
 namespace MMTDigitalTechTest.PageObjects
 {
     public class LoginPage
     {
         public IWebDriver driver;
-        WebDriverWait wait;
+        private WebDriverWait wait;
 
         public LoginPage(IWebDriver driver)
         {
@@ -18,12 +19,39 @@ namespace MMTDigitalTechTest.PageObjects
         private IWebElement usernameField => driver.FindElement(By.Id("user-name"));
         private IWebElement passwordField => driver.FindElement(By.Id("password"));
         private IWebElement loginBtn => driver.FindElement(By.Id("login-button"));
+        private string errorMsg => driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[1]/div[1]/div/form/div[3]/h3")).Text;
+        private string username = "standard_user";
+        private string password = "secret_sauce";
+
+        public void NoUsrNoPwd()
+        {
+            loginBtn.Click();
+        }
+
+        public void UserNoPwd()
+        {
+            usernameField.SendKeys(username);
+            loginBtn.Click();
+        }
+
+        public void IncorrectDetails()
+        {
+            usernameField.SendKeys("beep");
+            passwordField.SendKeys("boop");
+            loginBtn.Click();
+        }
 
         public void Login()
         {
-            usernameField.SendKeys("standard_user");
-            passwordField.SendKeys("secret_sauce");
+            usernameField.SendKeys(username);
+            passwordField.SendKeys(password);
             loginBtn.Click();
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id("item_4_img_link")));
+        }
+
+        public String pullErrorMsg()
+        {
+            return errorMsg;
         }
 
 
--- MMTDigitalTechTest/UserStories/AC1.cs
+++ MMTDigitalTechTest/UserStories/AC1.cs
@@ -21,14 +21,49 @@ namespace MMTDigitalTechTest.UserStories
             driver = InitialiseDriver();
         }
 
-        [Test]
-        public void validusrpwd()
+        [Test, Order(1)]
+        [Description("Scenario 1: Clicking 'Log in' with no username or password")]
+        public void UsernameRequiredScenario()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.NoUsrNoPwd();
+            //below the assert tries matching the expected error message against the error message of the element at each state.
+            Assert.AreEqual("Epic sadface: Username is required", loginPage.pullErrorMsg());
+            commonUtils.PrintLogs("browser", driver);
+        }
+
+        [Test, Order(2)]
+        [Description("Scenario 2: Username entered but no password")]
+        public void PasswordRequiredScenario()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.UserNoPwd();
+            //below the assert tries matching the expected error message against the error message of the element at each state.
+            Assert.AreEqual("Epic sadface: Password is required", loginPage.pullErrorMsg());
+            commonUtils.PrintLogs("browser", driver);
+        }
+
+        [Test, Order(3)]
+        [Description("Scenario 3: Incorrect details entered ")]
+        public void IncorrectDetailsScenario()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.IncorrectDetails();
+            //below the assert tries matching the expected error message against the error message of the element at each state.
+            Assert.AreEqual("Epic sadface: Username and password do not match any user in this service", loginPage.pullErrorMsg());
+            commonUtils.PrintLogs("browser", driver);
+        }
+
+
+
+        [Test, Order(4)]
+        [Description("Scenario 4: Entering Correct username and password directs user to product page")]
+        public void ValidDetailsScenario()
         {
             LoginPage loginPage = new LoginPage(driver);
             loginPage.Login();
-
-            
-
+            //As this is the correct details scenario, the assert checks to see if the URL of the page the user is redirected to is correct
+            //if false, it could mean the username and password was not accepted or the user faced another error
             Assert.IsTrue(driver.Url == "https://www.saucedemo.com/inventory.html");
             commonUtils.PrintLogs("browser", driver);
         }
