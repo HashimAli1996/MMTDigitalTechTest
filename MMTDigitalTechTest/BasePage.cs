1322176cd4311b01d9ec7bbacfdc8acb3c3ef45b
HashimAli1996 <hashim20070@hotmail.co.uk>
2021-04-25 05:07:58 +0100

    Created Framework, BasePage, PageShells

---
+++ MMTDigitalTechTest/BasePage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace MMTDigitalTechTest
+{
+
+    [TestFixture]
+    public class BasePage
+    {
+        public static IWebDriver driver;
+        private WebDriverWait wait;
+        public BasePage(IWebDriver driver)
+        {
+            BasePage.driver = driver;
+        }
+
+        public BasePage() { }
+
+        public void runDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArgument("disable-infobars");
+
+            driver = new ChromeDriver(options);
+        }
+
+        [TearDown]
+        public void cleanUp()
+        {
+            Console.WriteLine("cleaning driver....");
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        public void WaitForElementById(String id)
+        {
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(id)));
+        }
+
+        public void WaitForElement(String xpath)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(xpath)));
+        }
+
+        public void JavaScriptClick(IWebElement element)
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", element);
+        }
+
+        public void WaitForElementToBeClickable(String locator)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locator)));
+        }
+
+
+    }
+}
---
+++ MMTDigitalTechTest/PageObjects/AboutPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMTDigitalTechTest.PageObjects
+{
+    class AboutPage
+    {
+    }
+}
